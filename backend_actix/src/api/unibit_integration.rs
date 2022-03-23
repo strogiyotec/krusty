b51191359b38f7bf7c371b051d300f92184ddc6a
strogiyotec <almas337519@gmail.com>
2022-03-23 11:14:05 -0700

    change sector to industry

--- backend_actix/src/api/unibit_integration.rs
+++ backend_actix/src/api/unibit_integration.rs
@@ -9,8 +9,6 @@ use crate::router::stock_router::ErrorMessage;
 /// Integration with UniBit API
 /// to fetch the data by stock ticker
 /// see it's here https://unibit.ai/api/docs/V2.0/
-
-
 #[derive(Clone)]
 pub struct UniBitApi {
     api_token: String,
@@ -23,20 +21,18 @@ impl UniBitApi {
 
     pub async fn get_sector_by_ticker(&self, ticker: &String) -> Result<String, ErrorMessage> {
         let client = awc::Client::new();
-        let url = format!("https://api.unibit.ai/v2/company/profile?tickers={ticker}&selectedFields=sector,&accessKey={access_key}", access_key = self.api_token,ticker=ticker);
+        let url = format!("https://api.unibit.ai/v2/company/profile?tickers={ticker}&selectedFields=industry,&accessKey={access_key}", access_key = self.api_token, ticker = ticker);
         let response = client.get(url)
             .send()
             .await;
         return match response {
             Ok(mut response) => {
                 let response_body = response.json::<serde_json::Value>().await.expect("Can't get json on sector_by_ticket from UniBit API");
-                let str = response_body.to_string();
-                println!("{:?}",response_body.get("result_data"));
                 let sector = response_body.get("result_data")
                     .unwrap()
                     .get(ticker)
                     .unwrap()
-                    .get("sector")
+                    .get("industry")
                     .unwrap()
                     .as_str()
                     .unwrap();
